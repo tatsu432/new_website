6d900380c10bd4698b6cf27537c67b6c24b85111
Tatsuhiro Shimizu <t.miyazaki3831@gmail.com>
2022-03-29 13:37:53 +0900

    映画批評のファイルを追加し、ハンバーガーメニュ二そのファイルを追加

--- works/bbs.php
+++ works/bbs.php
@@ -67,6 +67,7 @@ fclose($fp);
                         <li><a class="drawer-dropdown-menu-item" target="_blank" href="./kendama_rec.html">Recommended Tricks</a></li>
                     </ul>
                     </li>
+                    <li><a class="drawer-menu-item" target="_blank" href="./film.html">Film reccomendation</a></li>
                     <li><a class="drawer-menu-item" target="_blank" href="http://shimizu432.php.xdomain.jp/works/bbs.php">Bulltein Board</a></li>
                 </ul>
             </nav>
